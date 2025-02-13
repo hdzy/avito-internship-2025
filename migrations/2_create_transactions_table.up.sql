ac0a2b30118d334af740fbe9fdff16c6d6e85e54
Smirnov Oleg <smirnov.oleg.1231@gmail.com>
2025-02-13 12:25:25 +0300

    fix: change transactions table

--- migrations/2_create_transactions_table.up.sql
+++ migrations/2_create_transactions_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     employee_id INTEGER REFERENCES employees(id),
     type VARCHAR(20) NOT NULL,  -- transfer / purchase
     amount INTEGER NOT NULL,
+    merch_id INTEGER REFERENCES merch_items(id), -- id мерча, если покупка мерча
     counterparty VARCHAR(50),   -- контрагент (другой сотрудник для перевода)
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
