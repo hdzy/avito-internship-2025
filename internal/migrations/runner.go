9b3924d24fb7a265077b79eb7fe000a2357aca10
Smirnov Oleg <smirnov.oleg.1231@gmail.com>
2025-02-14 14:55:22 +0300

    fix: remove unused comments

--- internal/migrations/runner.go
+++ internal/migrations/runner.go
@@ -3,8 +3,8 @@ package migrations
 import (
 	"errors"
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres" // импорт драйвера для PostgreSQL
-	_ "github.com/golang-migrate/migrate/v4/source/file"       // импорт источника файлов миграций
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"log"
 )
 
